e5faf2c1a9e26989a78cee4d30bd6af0a5c4e282
Daniel Lebrecht <dlebrecht@gmail.com>
2017-10-31 11:31:09 +0100

    cleaned imports

--- lib/request.ts
+++ lib/request.ts
@@ -2,11 +2,10 @@ import * as rxjsfetch from 'rxjs-fetch';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise'
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/concatMapTo';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/repeat';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/of';
 
 const rpcBody = (method) => (params) =>
   ({
